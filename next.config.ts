a4acee48bd20fd8583d99b9faae2d613cdac82d6
venkatesh <offlvenkatesh@gmail.com>
2026-03-30 16:51:19 +0530

    Revert "Update next.config.ts for static export"
    This reverts commit 645ac98e982a3dc778896be5965d288deb0427ed.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  output: 'export',
+  /* config options here */
   reactCompiler: true,
-  images: { unoptimized: true },
 };
+
 export default nextConfig;
